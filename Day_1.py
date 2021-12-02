0cf1852fa2cd965602ee9e1ca2d40ccbb9c79637
Jack <jrmaring@stu.naperville203.org>
2021-12-01 20:42:02 -0600

    Changed it to not be stupid

--- Day_1.py
+++ Day_1.py
@@ -6,15 +6,14 @@ while nums[-1]!=-1:
 nums=nums[1:len(nums)-1]
 count=0
 curr=nums[0]+nums[1]+nums[2]
-prev=float("inf")
+prev=curr
 
-for i in range(2, len(nums)):
-    curr=nums[i-2]+nums[i-1]+nums[i]
+for i in range(3, len(nums)):
+    curr+=nums[i]-nums[i-3]
 
     if curr>prev:
         count+=1
 
     prev=curr
-
     
 print(count)
