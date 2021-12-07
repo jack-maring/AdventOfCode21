19f21196d92f93707a96f9373ebabac82357fedc
Jack <jrmaring@stu.naperville203.org>
2021-12-06 23:23:11 -0600

    Changed from calculating the sum manually to using a formula because I forgot that existed

--- Day_7.py
+++ Day_7.py
@@ -1,5 +1,3 @@
-memo={} #After further testing memoization isn't really necessary here but the program didn't give me output instantly and I got scared
-
 #Make data
 def makeData():
     lines=open("Inputs/Day_7.txt", "r").read().split(",")
@@ -16,25 +14,12 @@ def solve(lines):
     for i in range(highest):
         count=0
         for j in range(len(lines)):
-            count+=sumAllLess(abs(lines[j]-i)) #For part 1 just get rid of the sumAllLess() part and make it just lines[j]-i
+            count+=abs(lines[j]-i)*(abs(lines[j]-i)+1)/2 #For part 1 just make it abs(lines[j]-i)
 
         if count<best:
             best=count
 
     return best
-
-
-def sumAllLess(n):
-    if n in memo:
-        return memo[n]
-
-    count=0
-
-    for i in range(n+1):
-        count+=i
-
-    memo[n]=count
-    return count
 
 
 #Calculate and display answer
