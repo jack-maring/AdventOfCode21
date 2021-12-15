eae83cc3f3b4a9123745276231a5bf681cce09dc
Jack <jrmaring@stu.naperville203.org>
2021-12-14 22:50:42 -0600

    Formatting

--- Day_14.py
+++ Day_14.py
@@ -1,29 +1,31 @@
-lines=open("Inputs/Day_14.txt", "r").read().split("\n")
+lines = open("Inputs/Day_14.txt", "r").read().split("\n")
 
-pairs={}
+pairs = {}
 for i in range(1, len(lines[0])):
-    pairs[lines[0][i - 1:i + 1]] = pairs[lines[0][i - 1:i + 1]]+1 if lines[0][i-1:i+1] in pairs else 1
+    pairs[lines[0][i - 1:i + 1]] = pairs[lines[0][i - 1:i + 1]] + 1 if lines[0][i - 1:i + 1] in pairs else 1
 
-inserts={}
+inserts = {}
 for i in range(2, len(lines)):
-    inserts[lines[i][:2]]=lines[i][6:]
+    inserts[lines[i][:2]] = lines[i][6:]
 
-counts={}
+counts = {}
 for i in lines[0]:
-    counts[i]=counts[i]+1 if i in counts else 1
+    counts[i] = counts[i] + 1 if i in counts else 1
 
-for i in range(40): #Change to 10 for part 1
-    newPairs={}
+for i in range(40):  # Change to 10 for part 1
+    newPairs = {}
     for key in inserts:
         if key in pairs:
-            counts[inserts[key]] = counts[inserts[key]] +pairs[key] if inserts[key] in counts else pairs[key]
-            newPairs[key[0] + inserts[key]] = newPairs[key[0] + inserts[key]]+ pairs[key] if key[0] + inserts[key] in newPairs else pairs[key]
-            newPairs[inserts[key]+key[1]] = newPairs[inserts[key]+key[1]] + pairs[key] if inserts[key]+key[1] in newPairs else pairs[key]
+            counts[inserts[key]] = counts[inserts[key]] + pairs[key] if inserts[key] in counts else pairs[key]
+            newPairs[key[0] + inserts[key]] = newPairs[key[0] + inserts[key]] + pairs[key] if key[0] + inserts[
+                key] in newPairs else pairs[key]
+            newPairs[inserts[key] + key[1]] = newPairs[inserts[key] + key[1]] + pairs[key] if inserts[key] + key[
+                1] in newPairs else pairs[key]
 
-    pairs=newPairs
+    pairs = newPairs
 
-highest, lowest=float("-inf"), float("inf")
+highest, lowest = float("-inf"), float("inf")
 for key in counts:
     highest, lowest = max(highest, counts[key]), min(lowest, counts[key])
 
-print(highest-lowest)
+print(highest - lowest)
